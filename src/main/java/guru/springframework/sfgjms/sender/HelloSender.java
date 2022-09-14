38a1a130d7140bae784e5993382881b1c27d3682
Comprehensher <boltankove@gmail.com>
2022-09-14 08:03:09 +0300

    Adding send and receive jms message example.

--- src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
+++ src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
@@ -1,24 +1,32 @@
 package guru.springframework.sfgjms.sender;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.sfgjms.config.JmsConfig;
 import guru.springframework.sfgjms.model.HelloWorldMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 import java.util.UUID;
 
+/**
+ * Created by jt on 2019-07-17.
+ */
 @RequiredArgsConstructor
 @Component
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
-    @Scheduled(fixedRate = 2000) /*send every 2 sec*/
-    public void sendMessage() {
-
-        System.out.println("I'm sending a message");
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage(){
 
         HelloWorldMessage message = HelloWorldMessage
                 .builder()
@@ -26,9 +34,40 @@ public class HelloSender {
                 .message("Hello World!")
                 .build();
 
-        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message); //send message to a queue AcrtiveMQ
-
-        System.out.println("Message Sent");
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 
     }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendandReceiveMessage() throws JMSException {
+
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello")
+                .build();
+
+        Message receviedMsg = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RCV_QUEUE, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                Message helloMessage = null;
+
+                try {
+                    helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                    helloMessage.setStringProperty("_type", "guru.springframework.sfgjms.model.HelloWorldMessage");
+
+                    System.out.println("Sending Hello");
+
+                    return helloMessage;
+
+                } catch (JsonProcessingException e) {
+                    throw new JMSException("boom");
+                }
+            }
+        });
+
+        System.out.println(receviedMsg.getBody(String.class));
+
+    }
+
 }
